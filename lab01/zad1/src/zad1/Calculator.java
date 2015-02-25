4b3c6d589bab7d39f7117a10003c4fd2a2cb387f
Marek Grabowski <grabowskimark@outlook.com>
2015-02-25 17:23:41 +0100

    Zadanie 1 a i b

---
+++ lab01/zad1/src/zad1/Calculator.java
@@ -0,0 +1,25 @@
+package zad1;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		
+	}
+	
+	public static int add(int a, int b){
+		return a+b;
+	}
+	
+	public static int sub(int a, int b){
+		return a-b;
+	}
+	
+	public static int multi(int a, int b){
+		return a*b;
+	}
+	
+	public static int div(int a, int b){
+		return a/b;
+	}
+
+}
